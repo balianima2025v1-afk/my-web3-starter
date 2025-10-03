9c738900dcbc591c675e2eb8000a8c1ebb378e11
balianima2025v1-afk <balianima2025v1@gmail.com>
2025-10-03 17:00:28 +0800

    Update register.js

--- pages/api/register.js
+++ pages/api/register.js
@@ -1,8 +1,8 @@
-import { createClient } from "@supabase/supabase-js"
+import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+const supabaseUrl = 'https://nicolbhswiggmqdacvud.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 )
 
 export default async function handler(req, res) {
